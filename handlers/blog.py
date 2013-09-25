edfd101207b5e1a37618d677511e5d0dc9b62f3b
Tyler Chung <zonyitoo@gmail.com>
2013-09-25 21:03:08 +0800

    fixed bug

--- handlers/blog.py
+++ handlers/blog.py
@@ -15,27 +15,28 @@ class BlogHandler(BaseHandler):
         if current_page >= page_cnt:
             current_page = page_cnt - 1
         
-        for ar in query_result[current_page * 5:(current_page + 1) * 5]:
-            article = {
-                    '_id': ar["_id"],
-                   'title': ar["title"],
-                   'date': ar["date"],
-                   'content': publish_parts(ar['content'], writer_name='html')['html_body'],
-                   'categories': self.db.category.find({"articles": ar['_id']}),
-                   'comment_count': self.db.comment.find({"article": ar['_id']}).count()
-               }
-            articles.append(article)
-
-        for ar in articles:
-           imgs = regx.findall(ar["content"])
-           if len(imgs) != 0:
-               carousels.append({
-                       '_id': ar["_id"],
+        if query_result.count() > 0:
+            for ar in query_result[current_page * 5:(current_page + 1) * 5]:
+                article = {
+                        '_id': ar["_id"],
                        'title': ar["title"],
-                       'img': imgs[0],
-                       'categories': article["categories"],
-                       'date': article["date"],
-                   })
+                       'date': ar["date"],
+                       'content': publish_parts(ar['content'], writer_name='html')['html_body'],
+                       'categories': self.db.category.find({"articles": ar['_id']}),
+                       'comment_count': self.db.comment.find({"article": ar['_id']}).count()
+                   }
+                articles.append(article)
+
+            for ar in articles:
+               imgs = regx.findall(ar["content"])
+               if len(imgs) != 0:
+                   carousels.append({
+                           '_id': ar["_id"],
+                           'title': ar["title"],
+                           'img': imgs[0],
+                           'categories': article["categories"],
+                           'date': article["date"],
+                       })
 
         self.render("blog.html", 
                 sidebar=self.get_sidebar_data(), articles=articles, carousels=carousels[:4],
